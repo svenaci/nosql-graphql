2f44d585f2e220b3f62e0899971f656bab481279
svenaci <steffan_v@hotmail.com>
2023-11-14 15:16:20 -0500

    hooking frontend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
+  const [hotels, setHotels] = useState(null);
+
+  const fetchData = async () => {
+    const response = await fetch(`.netlify/functions/getHotels`);
+    const responseBody = await response.json();
+    setHotels(responseBody.data.hotel_data.values);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(hotels);
+
   return <div className="App"></div>;
 };
 
